339c116853249ade9efada086a6dc6e0cd529fe4
kunirvik <123270547+kunirvik@users.noreply.github.com>
2024-04-17 16:58:07 +0000

    Deploying to gh-pages from @ kunirvik/new-page@d06b8bcf701162c0c3636c7bdbf44c5c92c52d68 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -2,15 +2,16 @@ import{b as f,g as s,S as d}from"./assets/vendor-5af63c29.js";(function(){const 
    
      <div class="modal">
   
-     <div class="container">
+     <div class="modal-container">
+     <button class="close-button">X</button>
      <h1>Наши контакты</h1>
      <div class="contact-info">
          <p><strong>Адрес:</strong> 123 улица, Город, Страна, Почтовый индекс</p>
-         <p><strong>Телефон:</strong> <span class="phone">+7 (123) 456-7890</span></p>
+         <p><strong>Телефон: +380961383642</strong> <span class="phone">+7 (123) 456-7890</span></p>
          <p><strong>Email:</strong> <a href="mailto:info@example.com" class="email">info@example.com</a></p>
      </div>
  </div>
     
     </div>
-    `,{handler:null,onShow(){this.handler=p.bind(n),document.addEventListener("keydown",this.handler)},onClose(){document.removeEventListener("keydown",this.handler)}});n.show()}s.registerPlugin(d);const c=document.querySelector(".layout"),r=document.querySelectorAll(".slide"),h=document.querySelector(".text"),a=c.offsetWidth;h.addEventListener("click",g);function g(n){n.preventDefault(),n.target.classList.contains("js-contact")&&m()}d.matchMedia({"(min-width: 800px)":function(){s.timeline({scrollTrigger:{markers:!0,trigger:c,pin:!0,scrub:2,start:"top 20",ease:"none",end:()=>"+="+a,anticipatePin:1,invalidateOnRefresh:!0}}).to(r,{xPercent:-110*(r.length-1)})},"(max-width: 799px) and (max-height: 899px)":function(){s.timeline({scrollTrigger:{markers:!0,trigger:c,pin:!0,scrub:1,start:"top 20",ease:"none",end:()=>"+="+a*2,anticipatePin:1,invalidateOnRefresh:!0}}).to(r,{xPercent:-110*(r.length-1)})}});
+    `,{handler:null,onShow(){this.handler=p.bind(n),document.addEventListener("keydown",this.handler)},onClose(){document.removeEventListener("keydown",this.handler)}});n.show()}s.registerPlugin(d);const c=document.querySelector(".layout"),r=document.querySelectorAll(".slide"),h=document.querySelector(".text"),a=c.offsetWidth;h.addEventListener("click",g);function g(n){n.preventDefault(),n.target.classList.contains("js-contact")&&m()}d.matchMedia({"(min-width: 800px)":function(){s.timeline({scrollTrigger:{markers:!0,trigger:c,pin:!0,scrub:2,start:"top 20",ease:"none",end:()=>"+="+a,anticipatePin:1,invalidateOnRefresh:!0}}).to(r,{xPercent:-110*(r.length-4)})},"(max-width: 799px) and (max-height: 899px)":function(){s.timeline({scrollTrigger:{markers:!0,trigger:c,pin:!0,scrub:1,start:"top ",ease:"none",end:()=>"+="+a*10,anticipatePin:1,invalidateOnRefresh:!0}}).to(r,{xPercent:-110*(r.length-1)})}});
 //# sourceMappingURL=commonHelpers.js.map
