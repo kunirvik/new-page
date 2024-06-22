e278c059c2c15358d9dca4560e7a140e65463a9f
kunirvik <123270547+kunirvik@users.noreply.github.com>
2024-06-22 13:35:52 +0300

    ggg

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,91 +1,77 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
 const images = [
-    {
+  {
       preview:
-        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
       original:
-        "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
       description: "Hokkaido Flower",
-    },
-    {
+  },
+  {
       preview:
-        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
       original:
-        "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
       description: "Container Haulage Freight",
-    },
-    {
+  },
+  {
       preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
       original:
-        "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+          "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
       description: "Aerial Beach View",
-    },
-    {
+  },
+  {
       preview:
-        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+          "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
       original:
-        "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+          "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
       description: "Flower Blooms",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-      description: "Alpine Mountains",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-      description: "Mountain Lake Sailing",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-      description: "Alpine Spring Meadows",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-      description: "Nature Landscape",
-    },
-    {
-      preview:
-        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-      original:
-        "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-      description: "Lighthouse Coast Sea",
-    },
-    
-    
-  ];
+  }
+];
 
-  const markup = images.map(({preview, original, description}) => 
-`<li class="gallery__item">
-<a class="gallery__link" href="${original}">
-  <img
-    class="gallery__image"
-    src="${preview}"
-    data-source="${original}"
-    alt="${description}"
-  />
-</a>
-</li>`).join('')
+// Generate HTML markup for each image
+const markup = images.map(({ preview, original, description }) => `
+  <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+          <div class="gallery__image-container">
+              <img
+                  class="gallery__image"
+                  src="${preview}"
+                  data-source="${original}"
+                  alt="${description}"
+              />
+          </div>
+      </a>
+  </li>
+`).join('');
 
+// Insert the markup into the .gallery element
 const gallery = document.querySelector('.gallery');
-
 gallery.insertAdjacentHTML('beforeend', markup);
 
-const lightbox = new SimpleLightbox('.gallery a', {
-    captions: true, 
-    captionsData: 'alt', 
-    captionDelay: 250, 
+// Add event listeners for hover effects
+document.querySelectorAll('.gallery__image').forEach(image => {
+  image.addEventListener('mouseover', function() {
+      this.classList.add('zoom');
+      const targetSelector = this.getAttribute('data-hover-target');
+      document.querySelector(targetSelector).classList.add('hover-effect');
+      
+      // Apply frosted glass effect to other galleries
+      document.querySelectorAll('.gallery').forEach(gallery => {
+          if (!gallery.contains(this.closest('.gallery__item'))) {
+              gallery.classList.add('frosted-glass');
+          }
+      });
   });
+
+  image.addEventListener('mouseout', function() {
+      this.classList.remove('zoom');
+      const targetSelector = this.getAttribute('data-hover-target');
+      document.querySelector(targetSelector).classList.remove('hover-effect');
+      
+      // Remove frosted glass effect from other galleries
+      document.querySelectorAll('.gallery').forEach(gallery => {
+          gallery.classList.remove('frosted-glass');
+      });
+  });
+});
