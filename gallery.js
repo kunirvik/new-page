7419b88c948f3c5b2bada6a39ade858b48026a8e
kunirvik <123270547+kunirvik@users.noreply.github.com>
2024-07-06 21:04:21 +0000

    Deploying to gh-pages from @ kunirvik/new-page@fa27db664a3acee63b19cda5b9bf279ade4c195e 🚀

--- gallery.js
+++ gallery.js
@@ -1,15 +1,8 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";const a=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",original:"https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",description:"Flower Blooms"}],r=a.map(({preview:o,original:e,description:t})=>`
-  <li class="gallery__item">
-      <a class="gallery__link" href="${e}">
-          <div class="gallery__image-container">
-              <img
-                  class="gallery__image"
-                  src="${o}"
-                  data-source="${e}"
-                  alt="${t}"
-              />
-          </div>
-      </a>
-  </li>
-`).join(""),c=document.querySelector(".gallery");c.insertAdjacentHTML("beforeend",r);const i=document.querySelector(".gallery__image");i.addEventListener("click",s);function s(o){}document.querySelectorAll(".gallery__image").forEach(o=>{o.addEventListener("mouseover",function(){this.classList.add("zoom");const e=this.getAttribute("data-hover-target");document.querySelector(e).classList.add("hover-effect"),document.querySelectorAll(".gallery").forEach(t=>{t.contains(this.closest(".gallery__item"))||t.classList.add("frosted-glass")})}),o.addEventListener("mouseout",function(){this.classList.remove("zoom");const e=this.getAttribute("data-hover-target");document.querySelector(e).classList.remove("hover-effect"),document.querySelectorAll(".gallery").forEach(t=>{t.classList.remove("frosted-glass")})})});
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{g as l}from"./assets/vendor-0fd547fe.js";function i(e,r,t,a){const o=`<div class="wrapper-description"><h1>${t}</h1><p>${a}</p></div>`,g=r.map(({preview:c})=>`
+        <li class="gallery__item">
+            <div class="gallery__image-container">
+                <img class="gallery__image" loading="lazy" src="${c}" />
+            </div>
+        </li>
+    `).join("");e.insertAdjacentHTML("beforeend",o+g)}const n=[{preview:"../img/6.jpg"},{preview:"../img/4.jpg"},{preview:"../img/7.jpg"},{preview:"../img/5.jpg"}],p=[{preview:"../img/3.jpg"},{preview:"../img/4.jpg"},{preview:"../img/9.jpg"},{preview:"../img/11.jpg"}],s=[{preview:"../img/12.jpg"},{preview:"../img/5.jpg"},{preview:"../img/4.jpg"},{preview:"../img/8.jpg"}],m=document.querySelector(".gallery1"),y=document.querySelector(".gallery2"),d=document.querySelector(".gallery3");i(m,n,"First Project","Description for Gallery 1");i(y,p,"Second Project","Description for Gallery 2");i(d,s,"Thirthd Project","Description for Gallery 3");document.querySelectorAll(".gallery__image").forEach(e=>{e.addEventListener("mouseenter",()=>{l.to(e,{scale:1.05,duration:.5})}),e.addEventListener("mouseleave",()=>{l.to(e,{scale:1,duration:.5})}),e.addEventListener("click",function(r){r.preventDefault(),r.target.closest(".gallery__item")&&(console.log("Navigating to page.html"),window.location.href="./gallery1.html")})});
 //# sourceMappingURL=gallery.js.map
